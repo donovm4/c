b4f035075ebb24b568703424a123c8cec6de73a4
Donovan McCoy <slycemccoy@gmail.com>
2026-01-05 14:03:30 -0500

    updating variables-related content

--- README.md
+++ README.md
@@ -1,13 +1,39 @@
 # C Programming
 
+C is a foundational pillar and is still important, even in the 2020s.
+
 ## Workflow
 
-### 1: Compile
-
-> [!NOTE]
->
-> as referred to as Build
+### 1: Compile (or build)
 
 ### 2: Run
 
 ## Variable types
+
+`int`: stores an integer (or whole number)
+
+```c
+int number = 2026;
+```
+
+`char`: stores a single character
+
+> [!IMPORTANT]
+>
+> `char <name-of-variable>[] allows for string/array of characters
+
+```c
+char fruit[] = "apple";
+```
+
+`double`: stores a decimal number
+
+```c
+double gpa = 3.85;
+```
+
+`float`: TBD
+
+```c
+float price = 237.99
+```
--- variables/main.c
+++ variables/main.c
@@ -3,10 +3,17 @@
 int main() 
 {
     // VARIABLE is a collection of characters or data
-    char fruit[] = "apple"; // variable of char type for string of characters
-    int number = 2026; // variable of int type for integer number
 
+    int number = 2026; // variable of int type for integer number
     printf("The year is %d.\n", number); // %d is for integers
+
+    char fruit[] = "apple"; // variable of char type for string of characters
     printf("My favorite fruit is %s.\n", fruit); // %s is for string of characters
+
+    double gpa = 3.85; // variable of double type for floating-point number
+    printf("Her GPA was %f in 2025.\n", gpa); // %f is for floating-point numbers
+    
+    char initial = 'D'; // variable of char type for single character
+    printf("My name starts with the letter %d.\n", initial); // %c is for single character
     return 0;
 }
