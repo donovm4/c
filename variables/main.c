cc91cb884097d5d214649f7f9e923b9785570c6d
Donovan McCoy <slycemccoy@gmail.com>
2026-01-05 13:01:13 -0500

    variables

---
+++ variables/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    // VARIABLE is a collection of characters or data
+    char fruit[] = "apple"; // variable of char type for string of characters
+    int number = 2026; // variable of int type for integer number
+
+    printf("The year is %d.\n", number); // %d is for integers
+    printf("My favorite fruit is %s.\n", fruit); // %s is for string of characters
+    return 0;
+}
